e7d0894d9cf3bb59170479e86cbd1106ad5eeb23
Huu Dang Pham <phamhuudang24122004@gmail.com>
2023-04-09 17:17:21 -0400

    Updates

--- backend/config/auth.js
+++
@@ -1,6 +0,0 @@
-const API_KEY =
-  "xsmtpsib-748f831d29a9f131f3ed28233d284dfd67007ccfa5c45c449aba171a58bb8cc7-szt5IkCbrLOdSYUE";
-const SENDER = "huudangphamt1k11@outlook.com";
-const PASSWORD = "@Phamhuudangt1k11";
-
-module.exports = { API_KEY, SENDER, PASSWORD };
