0e741b09080aab12a2d364382086c7c24e204984
Huu Dang Pham <phamhuudang24122004@gmail.com>
2023-04-24 02:53:07 -0400

    New changes

--- backend/service/mail.js
+++ backend/service/mail.js
@@ -3,12 +3,10 @@ const { API_KEY, SENDER, PASSWORD } = require("../config/auth");
 const SMTP_PORT = 465;
 const HOST_SERVICE = "smtp-relay.sendinblue.com";
 const transporter = nodemailer.createTransport({
-  host: HOST_SERVICE,
-  port: SMTP_PORT,
-  secure: true,
+  service: "outlook",
   auth: {
-    user: "phamhuudang24122004@gmail.com",
-    pass: "c6MYVr7QWBdPbqht",
+    user: "danghuupham@outlook.com",
+    pass: "@Phamhuudangt1k11",
   },
 });
 module.exports = { transporter };
